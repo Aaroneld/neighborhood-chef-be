3e4d0de9548dc893a515c0fdfed91cae0572f3c6
aaroneld <merrifield48@gmail.com>
2021-02-22 17:30:26 -0500

    add wakeup call for heroku

--- src/server.js
+++ src/server.js
@@ -1,51 +1,58 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const depthLimit = require('graphql-depth-limit');
-const cors = require('cors');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const depthLimit = require("graphql-depth-limit");
+const cors = require("cors");
 
-const authRouter = require('./routes/authrouter');
+const authRouter = require("./routes/authrouter");
 
-const typeDefs = require('./graphql/schemas');
-const resolvers = require('./graphql/resolvers');
-const mocks = require('./mocks');
-const authenticationRequired = require('./middleware/oktaAuthentication');
+const typeDefs = require("./graphql/schemas");
+const resolvers = require("./graphql/resolvers");
+const mocks = require("./mocks");
+const authenticationRequired = require("./middleware/oktaAuthentication");
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '15mb' }));
+app.use(express.json({ limit: "15mb" }));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
-const path = '/graphql'
+app.get("/wakeup", (req, res) => {
+  res.status(200).json({ awake: true });
+});
+
+const path = "/graphql";
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    mocks,
-    mockEntireSchema: false,
-    context: async ({ req }) => {
-        const token = req.headers.authorization;
+  typeDefs,
+  resolvers,
+  mocks,
+  mockEntireSchema: false,
+  context: async ({ req }) => {
+    const token = req.headers.authorization;
 
-        const authenticated = authenticationRequired(token);
+    const authenticated = authenticationRequired(token);
 
-        return { authenticated }
+    return { authenticated };
+  },
+  validationRules: [depthLimit(3)],
+
+  playground: {
+    path: path,
+    settings: {
+      "editor.theme": "dark",
     },
-    validationRules: [depthLimit(3)],
-
-    playground: {
-        path: path,
-        settings: {
-            'editor.theme': "dark"
-        }
-    }
+  },
 });
 
 server.applyMiddleware({ app, path });
