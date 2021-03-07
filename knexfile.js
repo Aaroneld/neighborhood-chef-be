de9ea20df206bf4a238761151727b8074f5cd737
patrick-replogle <patrickr1138@gmail.com>
2021-03-07 08:09:10 -0800

    revert connection string

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
         client: 'pg',
         connection:
             process.env.POSTGRES_CONNECTION_STRING ||
-            'postgres://postgres:Jellos1127@@localhost:5432/nhc_dev',
+            'postgres://postgres:1994@localhost:5434/nhc_dev',
         pool: {
             min: 0,
             max: 2,
