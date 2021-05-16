b89d1431b7f192f6002bb210718dba8a87500e46
aaroneld <merrifield48@gmail.com>
2021-05-16 15:00:31 -0400

    added-search-path-to-knex-config

--- knexfile.js
+++ knexfile.js
@@ -50,7 +50,6 @@ module.exports = {
     migrations: {
       directory: './data/migrations',
       tableName: 'knex_migrations',
-      schemaName: 'nhcredux-schema',
     },
     seeds: { directory: './data/seeds' },
   },
